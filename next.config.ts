5eae39a0668617ecc0462f43e3f95e78ac23819a
akcs-tvm <akcstvmdc@gmail.com>
2025-03-18 21:29:31 +0530

    Added filters

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // This specifies exporting the app as static content
-  distDir: 'akcs', // This will store the build files in the "akcs" folder
+  output: "export", // Export the app as static content
+  distDir: "akcs", // Store the build files in the "akcs" folder
+  exportPathMap: async function (defaultPathMap) {
+    // Exclude API routes from static export
+    delete defaultPathMap["/api/get-winner"];
+    delete defaultPathMap["/api/generate-winner"];
+    return defaultPathMap;
+  },
 };
 
 export default nextConfig;
-
